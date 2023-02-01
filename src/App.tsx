cb0cb4563c6462c489829938bf9da68af24befef
NeveSGG <neverovsg@gmail.com>
2023-02-01 22:05:21 +0600

    feat: header styling done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,81 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { observer } from 'mobx-react-lite';
-import rowsStore from 'stores/rowsStore';
+import { Theme } from '@mui/system';
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+  Paper
+} from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
 
-function getRandomInt(max: number): number {
-  return Math.floor(Math.random() * max);
-}
+import Main from 'routes/main';
+import Header from 'components/header';
+
+const darkTheme: Theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ffffff'
+      },
+      secondary: {
+        main: '#ffffff'
+      },
+      background: {
+        default: '#202124',
+        paper: '#27272A'
+      },
+      error: {
+        main: '#f44336'
+      },
+      text: {
+        primary: '#A1A1AA'
+      }
+    }
+  })
+);
 
 const App: FC = () => {
-  useEffect(() => {
-    rowsStore.getRows();
-  }, []);
-
-  const params = {
-    equipmentCosts: getRandomInt(5),
-    estimatedProfit: getRandomInt(5),
-    machineOperatorSalary: getRandomInt(5),
-    mainCosts: getRandomInt(5),
-    materials: getRandomInt(5),
-    mimExploitation: getRandomInt(5),
-    overheads: getRandomInt(5),
-    rowName: 'RANDOM',
-    salary: getRandomInt(5),
-    supportCosts: getRandomInt(5)
-  };
-
-  const addOne = async () => {
-    await rowsStore.addRow(params);
-    await rowsStore.getRows();
-  };
-
   return (
-    <div className="App">
-      <button onClick={() => addOne()}>Add row</button>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <ThemeProvider theme={darkTheme}>
+        <Grid2 container spacing={0}>
+          <Grid2 xs={12}>
+            <Paper sx={{ height: '44px', border: '1px solid #414144' }} square>
+              <Header />
+            </Paper>
+          </Grid2>
+          <Grid2 xs={2}>
+            <Paper
+              sx={{ height: '44px', border: '1px solid #414144' }}
+              square
+            ></Paper>
+          </Grid2>
+          <Grid2 xs={10}>
+            <Paper
+              sx={{ height: '44px', border: '1px solid #414144' }}
+              square
+            ></Paper>
+          </Grid2>
+          <Grid2 xs={2}>
+            <Paper
+              sx={{ height: 'calc(100vh - 88px)', border: '1px solid #414144' }}
+              square
+            ></Paper>
+          </Grid2>
+          <Grid2 xs={10}>
+            <Paper
+              sx={{ height: 'calc(100vh - 88px)', border: '1px solid #414144' }}
+              square
+            ></Paper>
+          </Grid2>
+        </Grid2>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
---
+++ src/routes/main/Main.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Main: FC = () => {
+  return <div></div>;
+};
+
+export default Main;
