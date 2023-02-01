3dd1d80c02c7cb2e7edcb36261afd7b519ca3d2d
NeveSGG <neverovsg@gmail.com>
2023-02-01 15:42:25 +0600

    feat: app`s initialized (eslint vite prettier ts)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  endOfLine: 'auto',
+  useTabs: false,
+  arrowParens: 'always'
+};
